0f6281e12e54fed9b5286db5f395d1f327eeb116
Gustavo Chen <gustavo.sao@gmail.com>
2022-05-02 01:45:42 -0300

    Make sass work

---
+++ src/components/Banner.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './../styles/banner.scss'
+
+const Banner:React.FC = () => {
+  return(
+    <div className='leftBlock'>Pimas</div>
+  )
+}
+
+export default Banner;
