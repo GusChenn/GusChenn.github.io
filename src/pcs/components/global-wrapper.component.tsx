3fcf6c33c2d832d518e5f7fe102c4ce2e84a6020
Gustavo Donnini Chen <gustavo.sao@gmail.com>
2022-06-21 21:00:04 -0300

    Create global wrapper style and add everything to page

--- src/pcs/components/global-wrapper.component.tsx
+++ src/pcs/components/global-wrapper.component.tsx
@@ -1,14 +1,18 @@
-import { Children } from 'react';
 import styled from 'styled-components';
 
 const GlobalWrapper= styled.div`
   margin: 0;
   padding: 0;
-  box-style: border-box;
+  box-sizing: border-box;
 
-  background: yellow;
-  height: 100vh;
-  width: 100vw;
+  background: #dddddd;
+  height: 100%;
+  width: 100%;
+
+  canvas {
+    height: 600px;
+  }
+  
 `;
 
 export default GlobalWrapper;
--- src/pcs/pcs-page.component.tsx
+++ src/pcs/pcs-page.component.tsx
@@ -1,14 +1,22 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import GlobalWrapper from './components/global-wrapper.component';
 import H1 from './components/h1.component';
+import Earth from './components/earth.component';
+import { OrbitControls } from '@react-three/drei';
 
 interface IPcsPageProps {};
 
 const PcsPage = ({}: IPcsPageProps): React.ReactElement => {
   return (
     <GlobalWrapper>
-      <H1 content='conteudo do h1' />
+      <H1 content='Trabalho PCS' />
+      <Canvas>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.4} />
+        <directionalLight color='white' position={[-1, 5, 2]} intensity={1} />
+        <Earth />
+      </Canvas>
     </GlobalWrapper>
   );
 };
